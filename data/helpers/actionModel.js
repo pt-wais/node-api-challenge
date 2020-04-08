a4266fbbaf208cec3324fe2f38b92f48ea38a243
Wais <wais.almakaleh@gmail.com>
2020-04-08 09:20:07 -0400

    ck

--- data/helpers/actionModel.js
+++ data/helpers/actionModel.js
@@ -1,5 +1,44 @@
-const db = require("../dbConfig.js");
-const mappers = require("./mappers");
+/* eslint-disable linebreak-style */
+const db = require('../dbConfig.js');
+const mappers = require('./mappers');
+
+
+function get(id) {
+  const query = db('actions');
+
+  if (id) {
+    return query
+      .where('id', id)
+      .first()
+      .then((action) => {
+        if (action) {
+          return mappers.actionToBody(action);
+        }
+        return null;
+      });
+  }
+  return query.then((actions) => actions.map((action) => mappers.actionToBody(action)));
+}
+
+function insert(action) {
+  return db('actions')
+    .insert(action)
+    .then(([id]) => get(id));
+}
+
+function update(id, changes) {
+  return db('actions')
+    .where('id', id)
+    .update(changes)
+    .then((count) => (count > 0 ? get(id) : null));
+}
+
+function remove(id) {
+  return db('actions')
+    .where('id', id)
+    .del();
+}
+
 
 module.exports = {
   get,
@@ -7,43 +46,3 @@ module.exports = {
   update,
   remove,
 };
-
-function get(id) {
-  let query = db("actions");
-
-  if (id) {
-    return query
-      .where("id", id)
-      .first()
-      .then(action => {
-        if (action) {
-          return mappers.actionToBody(action);
-        } else {
-          return null;
-        }
-      });
-  } else {
-    return query.then(actions => {
-      return actions.map(action => mappers.actionToBody(action));
-    });
-  }
-}
-
-function insert(action) {
-  return db("actions")
-    .insert(action)
-    .then(([id]) => get(id));
-}
-
-function update(id, changes) {
-  return db("actions")
-    .where("id", id)
-    .update(changes)
-    .then(count => (count > 0 ? get(id) : null));
-}
-
-function remove(id) {
-  return db("actions")
-    .where("id", id)
-    .del();
-}
